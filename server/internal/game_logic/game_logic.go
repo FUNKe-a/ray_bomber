e7033fc37c0b0d14e9c00a79982779ead421ce4a
FUNKe-a <codeberg.enslave061@passmail.com>
2026-09-18 01:28:06 +0300

    feat: thread for handling msg

--- server/internal/game_logic/game_logic.go
+++ server/internal/game_logic/game_logic.go
@@ -3,27 +3,33 @@ package gamelogic
 import "net"
 
 const (
-	emptyTile = iota
-	wallTile
+	EmptyTile = iota
+	WallTile
 )
 
 type Player struct {
 	ID   uint8
-	Conn net.Conn
-	X, Y int
+	X, Y uint8
 }
 
+// 20 values are reserved for tile types
 // 0 means empty tile
 type GameMatch struct {
-	Players map[uint8]*Player
-	Board   [15][13]uint8
+	Players map[net.Conn]*Player
+	Board   [][]uint8
 	IdCount uint8
 }
 
-func CreateMatch() GameMatch {
-	return GameMatch{Players: make(map[uint8]*Player), Board: [15][13]uint8{}, IdCount: 20}
+func CreateMatch(sizeX uint8, sizeY uint8) GameMatch {
+	board := make([][]uint8, sizeX)
+	for i := range board {
+		board[i] = make([]uint8, sizeY)
+	}
+	return GameMatch{Players: make(map[net.Conn]*Player), Board: board, IdCount: 20}
 }
 
-func (gm *GameMatch) ValidateMove(playerID uint8, direction uint8) (x, y uint8) {
-	return 0, 0
+func (board *GameMatch) Broadcast(data []byte) {
+	for k, _ := range board.Players {
+		k.Write(data)
+	}
 }
--- server/server.go
+++ server/server.go
@@ -1,41 +1,84 @@
 package main
 
 import (
-	"fmt"
-	// "github.com/FUNKe-a/ray_bomber/server/internal/game_logic"
+	"github.com/FUNKe-a/ray_bomber/server/internal/game_logic"
 	"github.com/FUNKe-a/ray_bomber/server/internal/net_io"
 	"github.com/FUNKe-a/ray_bomber/server/internal/protocol"
 	"github.com/FUNKe-a/ray_bomber/server/internal/serdes"
+	"log/slog"
 	"net"
 )
 
+type TraceableMessage struct {
+	Conn net.Conn
+	Msg  protocol.Message
+}
+
+func messageHandler(match *gamelogic.GameMatch, c <-chan TraceableMessage) {
+	for bundle := range c {
+		switch msg := bundle.Msg.Data.(type) {
+		case protocol.MoveRequested:
+			newX := match.Players[bundle.Conn].X
+			newY := match.Players[bundle.Conn].Y
+
+			switch msg.Direction {
+			case 0:
+				newY += 1
+			case 1:
+				newX += 1
+			case 2:
+				newY -= 1
+			case 3:
+				newX -= 1
+			}
+
+			if newX > 0 && newX < uint8(len(match.Board)) && newY > 0 && newY < uint8(len(match.Board[0])) {
+				if match.Board[newX][newY] == gamelogic.EmptyTile {
+					sendMsg := protocol.Message{Type: 0, Data: protocol.PlayerMoved{ID: match.Players[bundle.Conn].ID, X: newX, Y: newY}}
+					serialized, _ := serdes.Serialize(sendMsg)
+					match.Broadcast(serialized)
+				}
+			}
+		}
+	}
+}
+
 func main() {
-	// match := gamelogic.CreateMatch()
+	match := gamelogic.CreateMatch(15, 13)
+
+	var idCounter uint8 = 20
 
 	ln, _ := net.Listen("tcp", "127.0.0.1:6769")
+	slog.Info("TCP socket opened on 127.0.0.1:6769")
 
-	h_channel := make(chan protocol.Message)
+	h_channel := make(chan TraceableMessage)
 
 	for {
 		conn, _ := ln.Accept()
 
-		go func(player_conn net.Conn, handler_c chan protocol.Message) {
+		// TODO fix problem that idCounter will overfill
+		// if people will join and leave
+		match.Players[conn] = &gamelogic.Player{ID: idCounter}
+		greetMsg := protocol.Message{Type: 0, Data: protocol.Greeting{ID: idCounter}}
+		greetInBytes, _ := serdes.Serialize(greetMsg)
+		conn.Write(greetInBytes)
 
+		go func(player_conn net.Conn, handler_c chan<- TraceableMessage) {
 			for {
 				head, body, err := netio.ReadMessage(player_conn)
 				if err != nil {
-					fmt.Println(err)
-					continue
-				}
-				msg, err := serdes.Deserialize(head, body)
-				if err != nil {
-					fmt.Println(err)
 					continue
 				}
 
-				msgInBytes, err := serdes.Serialize(msg)
-				player_conn.Write(msgInBytes)
+				msg, err := serdes.Deserialize(head, body)
+				if err != nil {
+					continue
+				}
+				slog.Info("Message received.", "msgType", msg.Type)
+
 			}
 		}(conn, h_channel)
+
+		idCounter += 1
 	}
 }
